4fd1cd987fddf74abbf74f39cbeef64110ff056c
JornDeBruin1 <jorn.bruin@student.graafschapcollege.nl>
2023-06-01 12:46:09 +0200

    database conection files

---
+++ src/db.php
@@ -0,0 +1,30 @@
+<?php
+require_once('config/db_config.php');
+
+class DB
+{
+
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function query($sql)
+    {
+        $result = $this->conn->query($sql);
+        if (!$result) {
+            die("Query failed: " . $this->conn->error);
+        }
+        return $result;
+    }
+
+    public function __destruct()
+    {
+        $this->conn->close();
+    }
+}
